43dee0205ab281278068c1a2667f99cb84446ae6
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-12-03 14:59:34 +0100

    add

--- src/data/posts.jsx
+++
@@ -1,8 +0,0 @@
-export default [
-  { id: 1, title: "html", author: "alessandro", state: "published" },
-  { id: 2, title: "css", author: "michele", state: "published" },
-  { id: 3, title: "javascript", author: "michele", state: "published" },
-  { id: 4, title: "node.js", author: "gianni", state: "published" },
-  { id: 5, title: "react", author: "paolo", state: "published" },
-  { id: 6, title: "express", author: "eustorgio", state: "published" },
-];
